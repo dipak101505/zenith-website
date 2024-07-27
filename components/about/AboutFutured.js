b13764f50dad73b78fafe336132471a767e2386c
Dipak Agarwal <dipak.agarwal@deeeptek.ai>
2024-07-27 14:50:23 +0530

    first commit

---
+++ components/about/AboutFutured.js
@@ -0,0 +1,57 @@
+import FuturedItem from './Futured/FuturedItem';
+
+const AboutFutured = () => {
+  return (
+    <>
+      <div className="featured-area positioning  pb-76">
+        <div className="container">
+          <div className="row">
+            <div className="col-xxl-12 col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
+              <div
+                className="hrdd-section-title text-center wow animate fadeInDown"
+                data-wow-delay="100ms"
+                data-wow-duration="1500ms"
+              >
+                <h1>
+                  <span>Courses</span> We Provide
+                </h1>
+              </div>
+            </div>
+          </div>
+          <div className="row justify-content-center mt-50">
+            <FuturedItem
+              icon="/assets/images/icon/featured-icon-1.png"
+              title="IIT JEE"
+              desc="Assertively parallel task synergistic deliverables after high-quality"
+            />
+
+            <FuturedItem
+              icon="/assets/images/icon/featured-icon-2.png"
+              title="NEET"
+              desc="Assertively parallel task synergistic deliverables after high-quality"
+            />
+
+            <FuturedItem
+              icon="/assets/images/icon/featured-icon-3.png"
+              title="WBJEE"
+              desc="Assertively parallel task synergistic deliverables after high-quality"
+            />
+
+            <FuturedItem
+              icon="/assets/images/icon/featured-icon-4.png"
+              title="Class 7-10"
+              desc="Assertively parallel task synergistic deliverables after high-quality"
+            />
+
+            <FuturedItem
+              icon="/assets/images/icon/featured-icon-5.png"
+              title="Olympiad"
+              desc="Assertively parallel task synergistic deliverables after high-quality"
+            />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+export default AboutFutured;
