587b795d7c64ecca8c444423f715dd52a09f6ca7
rajdeepsaha2003 <rajdeepsahalkenzo@gmail.com>
2025-04-07 11:34:00 +0530

    Updated EducationHeader and Header in header branch

--- components/layout/EducationHeader.js
+++ components/layout/EducationHeader.js
@@ -41,8 +41,8 @@ return(
 <div className="row align-items-center">
     <div className="col-xxl-3 col-xl-2 col-lg-2 col-sm-6 col-6">
         <div className="logo text-left">
-            <Link href="/">
-                <a><Image src={NavConstant.Logo} alt="" width="173" height="50px"/></a>
+            <Link href="/"legacyBehavior>
+                <a><Image src={NavConstant.Logo} alt="" width="173" height="50"/></a>
             </Link>    
         </div>
     </div>
@@ -54,52 +54,65 @@ return(
         </div>
         <nav className="main-nav" ref={navToggle}> 
             <div className="logo mobile-ham-logo d-lg-none d-block text-left">
-                <Link href="/">
-                    <a><Image src={NavConstant.Logo} alt="" width="173" height="50px"/></a>
+                <Link href="/"legacyBehavior>
+                    <a><Image src={NavConstant.Logo} alt="" width="173" height="50"/></a>
                 </Link>    
             </div>
             <ul>
                 <li className="has-child-menu">
-                    <Link href="/"><a className="active">Home</a></Link>
+                    <Link href="/"legacyBehavior><a className="active">Home</a></Link>
                     <i className="fl flaticon-plus"  onClick={ToggleSubMenu}>+</i>
                     <ul className="sub-menu">
-                        <li><Link href="https://lms-eight-chi.vercel.app"><a>Online Course</a></Link></li>
-                        <li><Link href="https://zenithinstitute.theonlinetests.com/"><a>Online Test</a></Link></li>
-                        {/* <li><Link href="/education"><a>Education</a></Link></li>
-                        <li><Link href="/job"><a>Job Finding</a></Link></li>
-                        <li><Link href="/agency"><a>Digital Agency</a></Link></li> */}
+                        <li><Link href="https://lms-eight-chi.vercel.app"legacyBehavior><a>Online Course</a></Link></li>
+                        <li><Link href="https://zenithinstitute.theonlinetests.com/"legacyBehavior><a>Online Test</a></Link></li>
+                        {/* <li><Link href="/education"legacyBehavior><a>Education</a></Link></li>
+                        <li><Link href="/job"legacyBehaviorlegacyBehavior><a>Job Finding</a></Link></li>
+                        <li><Link href="/agency"legacyBehavior><a>Digital Agency</a></Link></li> */}
                     </ul>
                 </li>
                 <li className="has-child-menu">
-                    <Link href="/about"><a >About</a></Link>
+                    <Link href="/about"legacyBehavior><a >About</a></Link>
                     <i className="fl" >+</i>
                     <ul className="sub-menu">
-                        <li><Link href="/about/howrah"><a>Howrah</a></Link></li>
-                        <li><Link href="/about/Serampore"><a>Serampore</a></Link></li>
-                        <li><Link href="/about/andul"><a>Andul</a></Link></li>
-                        <li><Link href="/about/phoolbagan"><a>Phoolbagan</a></Link></li>
-                        <li><Link href="/about/saltlake"><a>Salt Lake</a></Link></li>
-                        <li><Link href="/about/chandannagar"><a>Chandannagar</a></Link></li>
-                        <li><Link href="/about/mogra"><a>Mogra</a></Link></li>
-                        <li><Link href="/about/rampurHat"><a>Rampurhat</a></Link></li>
-                        <li><Link href="/about/Raiganj"><a>Raiganj</a></Link></li>
-                        <li><Link href="/about/Balurghat"><a>Balurghat</a></Link></li>
+                        <li><Link href="/about/howrah"legacyBehavior><a>Howrah</a></Link></li>
+                        <li><Link href="/about/Serampore"legacyBehavior><a>Serampore</a></Link></li>
+                        <li><Link href="/about/andul"legacyBehavior><a>Andul</a></Link></li>
+                        <li><Link href="/about/phoolbagan"legacyBehavior><a>Phoolbagan</a></Link></li>
+                        <li><Link href="/about/saltlake"legacyBehavior><a>Salt Lake</a></Link></li>
+                        <li><Link href="/about/chandannagar"legacyBehavior><a>Chandannagar</a></Link></li>
+                        <li><Link href="/about/mogra"legacyBehavior><a>Mogra</a></Link></li>
+                        <li><Link href="/about/rampurHat"legacyBehavior><a>Rampurhat</a></Link></li>
+                        <li><Link href="/about/viproad"legacyBehavior><a>VIP Road</a></Link></li>
+                        <li><Link href="/about/dunlop"legacyBehavior><a>Dunlop</a></Link></li>
+                        <li><Link href="/about/Raiganj"legacyBehavior><a>Raiganj</a></Link></li>
+                        <li><Link href="/about/Balurghat"legacyBehavior><a>Balurghat</a></Link></li>
                     </ul>
                 </li>
-                <li><Link href="/services"><a>Courses</a></Link></li>
-                <li><Link href="/results"><a>Results</a></Link></li>
+                <li className="has-child-menu">
+                    <Link href="/about"legacyBehavior><a >Courses</a></Link>
+                    <i className="fl" >+</i>
+                    <ul className="sub-menu">
+                        <li><Link href="/IITJEE"legacyBehavior><a>IIT JEE</a></Link></li>
+                        <li><Link href="/NEET"legacyBehavior><a>NEET</a></Link></li>
+                        <li><Link href="/robotics"legacyBehavior><a>Robotics & AI</a></Link></li>
+                        <li><Link href="/foundation"legacyBehavior><a>Foundation Class 7-10</a></Link></li>
+                        <li><Link href="/"legacyBehavior><a>Olympiad</a></Link></li>
+                    </ul>
+                </li>
+                
+                <li><Link href="/results"legacyBehavior><a>Results</a></Link></li>
                 <li className="has-child-menu">
                     <a>Blog</a>
                     <i className="fl flaticon-plus" onClick={ToggleSubMenu}>+</i>
                     <ul className="sub-menu">
-                        <li><Link href="/"><a>Blog</a></Link></li>
-                        <li><Link href="/"><a>Blog Details</a></Link></li>
+                        <li><Link href="/"legacyBehavior><a>Blog</a></Link></li>
+                        {/* <li><Link href="/"legacyBehavior><a>Blog Details</a></Link></li> */}
                     </ul>
                 </li>
-                <li><Link href="/contact"><a>Contact </a></Link></li>
+                <li><Link href="/contact"legacyBehavior><a>Contact </a></Link></li>
             </ul>
             <div className="menu-btn-wrap d-block d-lg-none">
-                <Link href="/contact"><a className="edu-btn">Contact</a></Link>
+                <Link href="/contact"legacyBehavior><a className="edu-btn">Contact</a></Link>
             </div>
         </nav>
     </div>
