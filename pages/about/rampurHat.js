0a361bfc30d44246f0e1d829a35b4b0c904212e7
rajdeepsaha2003 <rajdeepsahalkenzo@gmail.com>
2025-06-24 08:13:19 +0530

    Fixed the problem of rampurhat page

--- pages/about/rampurHat.js
+++ pages/about/rampurHat.js
@@ -12,34 +12,34 @@ import RphTeacher from '../../components/TeacherCenterWise/RphTeacher';
 import Head from 'next/head';
 
 const Services = () => {
-  return (
-    <><Head>
-        <title>Zenith Rampurhat – JEE, NEET & Robotics Coaching Near Medical College</title>
-        <meta name="description" content="Coaching in IIT-JEE, NEET, Foundation & Robotics at Zenith Rampurhat – New Hospital Road."/>
-        <meta name="keywords" content="Rampurhat coaching, IIT NEET Rampurhat, Robotics Rampurhat, Zenith Institute Rampurhat, JEE preparation Rampurhat"/>
-        <meta property="og:image" content="https://firebasestorage.googleapis.com/v0/b/cloud-profiler-demo-399610.appspot.com/o/zenith-website%2Fins1.png?alt=media&token=1ee73676-393f-4a21-a614-2a799f55543e" />
-        <meta property="og:image:width" content="92" />
-        <meta property="og:image:height" content="92" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta charSet="UTF-8" />
-      </Head>
-      <header>
-        <Header />
-        <BreadCrumb pagename="Zenith Rampurhat" />
-      </header>
-      <SolutionArea 
-        title="Welcome to Zenith <span>Rampurhat</span>"
-        image="/assets/images/about/Rampurhat.png"
-        description="Zenith Rampurhat "
-      />
-      <AboutFutured />
-      <RphTeacher/>
-      {/* <ServicePricing /> */}
-      <AllCounter />
-      <TestimonialClient />
-      <ServiceNewsletter />
-      <Footer />
-    </>
-  );
+    return (
+        <><Head>
+            <title>Zenith Rampurhat – JEE, NEET & Robotics Coaching Near Medical College</title>
+            <meta name="description" content="Coaching in IIT-JEE, NEET, Foundation & Robotics at Zenith Rampurhat – New Hospital Road."/>
+            <meta name="keywords" content="Rampurhat coaching, IIT NEET Rampurhat, Robotics Rampurhat, Zenith Institute Rampurhat, JEE preparation Rampurhat"/>
+            <meta property="og:image" content="https://firebasestorage.googleapis.com/v0/b/cloud-profiler-demo-399610.appspot.com/o/zenith-website%2Fins1.png?alt=media&token=1ee73676-393f-4a21-a614-2a799f55543e" />
+            <meta property="og:image:width" content="92" />
+            <meta property="og:image:height" content="92" />
+            <meta name="viewport" content="width=device-width, initial-scale=1" />
+            <meta charSet="UTF-8" />
+        </Head>
+            <header>
+                <Header />
+                <BreadCrumb pagename="Zenith Rampurhat" />
+            </header>
+            <SolutionArea
+                title="Welcome to Zenith <span>Rampurhat</span>"
+                image="/assets/images/about/Rampurhat.png"
+                description="Zenith Rampurhat "
+            />
+            <AboutFutured />
+            <RphTeacher/>
+            {/* <ServicePricing /> */}
+            <AllCounter />
+            <TestimonialClient />
+            <ServiceNewsletter />
+            <Footer />
+        </>
+    );
 };
 export default Services;
