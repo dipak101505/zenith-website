401ce1b9c2f24a52b61a28bfd829e4990439460e
rajdeepsaha2003 <rajdeepsahalkenzo@gmail.com>
2025-06-30 13:12:02 +0530

    Changed some Styles

--- components/AboutWork/Item.js
+++ components/AboutWork/Item.js
@@ -5,13 +5,13 @@ return(
 <>
 <div className="latest-work-img-wrap">
 <img className="w-100" src={props.img} alt=""/>
-<div className="latest-work-hover-content d-flex align-items-end">
-    <div className="latest-work-hover-child">
-    <p>{props.name}</p>
-    <h2>{props.title}</h2>
-    {/* <Link href="/"><a>Read More <i className="bi bi-arrow-right"></i></a></Link> */}
-    </div>
-</div>
+{/*<div className="latest-work-hover-content d-flex align-items-end">*/}
+{/*    <div className="latest-work-hover-child">*/}
+{/*    <p>{props.name}</p>*/}
+{/*    <h2>{props.title}</h2>*/}
+{/*    /!* <Link href="/"><a>Read More <i className="bi bi-arrow-right"></i></a></Link> *!/*/}
+{/*    </div>*/}
+{/*</div>*/}
 </div>
 </>
 )
