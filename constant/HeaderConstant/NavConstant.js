d4bbb7e3817d51d59ac4451a183ac37eeded49fd
developmentdiginamicit <159012746+developmentdiginamicit@users.noreply.github.com>
2025-08-13 14:06:27 +0530

    Update NavConstant.js

--- constant/HeaderConstant/NavConstant.js
+++ constant/HeaderConstant/NavConstant.js
@@ -1,5 +1,6 @@
 const NavConstant = {
-  Logo: '/assets/images/image.png',
-  LogoMobile: '/assets/images/image.png',
+  Logo: "/assets/images/image.png",
+  LogoMobile: "/assets/images/image.png",
+  LogoLinkedIn: "/assets/images/logo-linkedin.png", // Add optimized LinkedIn logo
 };
 export default NavConstant;
