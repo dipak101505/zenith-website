9f9703cb2d42e4bc6935350d93f1a3c6e614a69e
developmentdiginamicit <159012746+developmentdiginamicit@users.noreply.github.com>
2025-08-21 12:55:06 +0530

    Update footer.js (#67)

--- components/layout/footer.js
+++ components/layout/footer.js
@@ -1,123 +1,299 @@
-import Link from "next/link"
-import FooterConstant from "../../constant/FooterConstant/FooterConstant"
+import Link from "next/link";
+import FooterConstant from "../../constant/FooterConstant/FooterConstant";
 
-const Footer=()=>{
-    
-return (
+const Footer = () => {
+  return (
     <div className="footer-area hrrd-footer-area positioning pb-100">
-        <div className="container">
+      <div className="container">
         <div className="row">
-        <div className="col-xl-12 col-lg-12 col-sm-12 col-12">
-        <div className="footer-wrap">
-        <div className="row justify-content-between">
-            <div className="col-xl-3 col-lg-4 col-md-12 col-sm-12 col-12">
-                <div className="single-widget">
+          <div className="col-xl-12 col-lg-12 col-sm-12 col-12">
+            <div className="footer-wrap">
+              <div className="row justify-content-between">
+                <div className="col-xl-3 col-lg-4 col-md-12 col-sm-12 col-12">
+                  <div className="single-widget">
                     <div className="footer-logo">
-                        <Link href="/"legacyBehavior>
-                        <a><img src={FooterConstant.Logo} alt=""/></a>
-                        </Link>
-                        <p>{FooterConstant.subTitle}</p>
-                        <div className="footer-social wow animate flipInX" data-wow-delay="200ms" data-wow-duration="1500ms">
-                            <ul>
-                                <li><Link href={FooterConstant.GoogleLink}legacyBehavior><a><i className="bi bi-google"></i></a></Link></li>
-                                <li><Link href={FooterConstant.LinkedinLink}legacyBehavior><a><i className="bi bi-linkedin"></i></a></Link></li>
-                                <li><Link href={FooterConstant.TwitterLink}legacyBehavior><a><i className="bi bi-youtube"></i></a></Link></li>
-                                <li><Link href={FooterConstant.InstagramLink}legacyBehavior><a><i className="bi bi-instagram"></i></a></Link></li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div className="col-xl-2 col-lg-2 col-md-6 col-sm-6 col-6">
-                <div className="single-widget">
-                    <div className="footer-title">
-                        <h3>Courses</h3>
-                    </div>
-                    <div className="footer-link">
+                      <Link href="/" legacyBehavior>
+                        <a>
+                          <img src={FooterConstant.Logo} alt="" />
+                        </a>
+                      </Link>
+                      <p>{FooterConstant.subTitle}</p>
+                      <div
+                        className="footer-social wow animate flipInX"
+                        data-wow-delay="200ms"
+                        data-wow-duration="1500ms"
+                      >
                         <ul>
-                        <li><Link href="/IITJEE"legacyBehavior><a>IIT JEE</a></Link></li>
-                        <li><Link href="/WBJEE"legacyBehavior><a>WBJEE</a></Link></li>
-                        <li><Link href="/NEET"legacyBehavior><a>NEET</a></Link></li>
-                        <li><Link href="/foundation"legacyBehavior><a>Foundation</a></Link></li>
-                        <li><Link href="/robotics" legacyBehavior><a>Robotics & AI</a></Link></li>
-                        <li><Link href="/olympiad"legacyBehavior><a>Olympiads - SOF</a></Link></li>
+                          <li>
+                            <Link
+                              href={FooterConstant.GoogleLink}
+                              legacyBehavior
+                            >
+                              <a>
+                                <i className="bi bi-google"></i>
+                              </a>
+                            </Link>
+                          </li>
+                          <li>
+                            <Link
+                              href={FooterConstant.LinkedinLink}
+                              legacyBehavior
+                            >
+                              <a>
+                                <i className="bi bi-linkedin"></i>
+                              </a>
+                            </Link>
+                          </li>
+                          <li>
+                            <Link
+                              href={FooterConstant.FacebookLink}
+                              legacyBehavior
+                            >
+                              <a>
+                                <i className="bi bi-facebook"></i>
+                              </a>
+                            </Link>
+                          </li>
+                          <li>
+                            <Link
+                              href={FooterConstant.TwitterLink}
+                              legacyBehavior
+                            >
+                              <a>
+                                <i className="bi bi-youtube"></i>
+                              </a>
+                            </Link>
+                          </li>
+                          <li>
+                            <Link
+                              href={FooterConstant.InstagramLink}
+                              legacyBehavior
+                            >
+                              <a>
+                                <i className="bi bi-instagram"></i>
+                              </a>
+                            </Link>
+                          </li>
                         </ul>
+                      </div>
                     </div>
+                  </div>
                 </div>
-            </div>
-            <div className="col-xl-2 col-lg-2 col-md-6 col-sm-6 col-6">
-                <div className="single-widget">
+                <div className="col-xl-2 col-lg-2 col-md-6 col-sm-6 col-6">
+                  <div className="single-widget">
                     <div className="footer-title">
-                        <h3>Company</h3>
+                      <h3>Courses</h3>
                     </div>
                     <div className="footer-link">
-                        <ul>
-                            <li><Link href="/"legacyBehavior><a>About</a></Link></li>
-                            <li><Link href="/"legacyBehavior><a>Results</a></Link></li>
-                            <li><Link href="/"legacyBehavior><a>Franchisee</a></Link></li>
-                            <li><Link href="/footerpages/job-openings"legacyBehavior><a>Job Openings</a></Link></li>
-                            <li><Link href="/footerpages/privacypolicy"legacyBehavior><a>Privacy Policy</a></Link></li>
-                            <li><Link href="/footerpages/school-tie-up"legacyBehavior><a>School Tie-up</a></Link></li>
-                        </ul>
+                      <ul>
+                        <li>
+                          <Link href="/IITJEE" legacyBehavior>
+                            <a>IIT JEE</a>
+                          </Link>
+                        </li>
+                        <li>
+                          <Link href="/WBJEE" legacyBehavior>
+                            <a>WBJEE</a>
+                          </Link>
+                        </li>
+                        <li>
+                          <Link href="/NEET" legacyBehavior>
+                            <a>NEET</a>
+                          </Link>
+                        </li>
+                        <li>
+                          <Link href="/foundation" legacyBehavior>
+                            <a>Foundation</a>
+                          </Link>
+                        </li>
+                        <li>
+                          <Link href="/robotics" legacyBehavior>
+                            <a>Robotics & AI</a>
+                          </Link>
+                        </li>
+                        <li>
+                          <Link href="/olympiad" legacyBehavior>
+                            <a>Olympiads - SOF</a>
+                          </Link>
+                        </li>
+                      </ul>
                     </div>
+                  </div>
                 </div>
-            </div>
-            <div className="col-xl-2 col-lg-6 col-md-6 col-sm-6 col-6">
-                <div className="single-widget">
+                <div className="col-xl-2 col-lg-2 col-md-6 col-sm-6 col-6">
+                  <div className="single-widget">
                     <div className="footer-title">
-                        <h3>Centres</h3>
+                      <h3>Company</h3>
                     </div>
                     <div className="footer-link">
-                        <ul>
-                        <li><Link href="/about/saltlake">Saltlake</Link></li>
-                            <li><Link href="/about/phoolbagan"legacyBehavior><a>Phoolbagan</a></Link></li>
-                            <li><Link href="/about/howrah"legacyBehavior><a>Howrah</a></Link></li>
-                            <li><Link href="/about/andul"legacyBehavior><a>Andul</a></Link></li>
-                            <li><Link href="/about/Serampore"legacyBehavior><a>Serampore</a></Link></li>
-                            <li><Link href="/about/chandannagar"legacyBehavior><a>Chandannagar</a></Link></li>
-                            <li><Link href="/about/mogra"legacyBehavior><a>Mogra</a></Link></li>
-                            <li><Link href="/about/Rampurhat"legacyBehavior><a>Rampurhat</a></Link></li>
-                            <li><Link href="/about/viproad"legacyBehavior><a>VIP Road</a></Link></li>
-                            <li><Link href="/about/dunlop"legacyBehavior><a>Dunlop</a></Link></li>
-                            <li><Link href="/about/Raiganj"legacyBehavior><a>Raiganj</a></Link></li>
-                            <li><Link href="/about/Balurghat"legacyBehavior><a>Balurghat</a></Link></li>
-                            <li><Link href="/about/Berhampore"legacyBehavior><a>Berhampore</a></Link></li>
+                      <ul>
+                        <li>
+                          <Link href="/" legacyBehavior>
+                            <a>About</a>
+                          </Link>
+                        </li>
+                        <li>
+                          <Link href="/" legacyBehavior>
+                            <a>Results</a>
+                          </Link>
+                        </li>
+                        <li>
+                          <Link href="/" legacyBehavior>
+                            <a>Franchisee</a>
+                          </Link>
+                        </li>
+                        <li>
+                          <Link href="/footerpages/job-openings" legacyBehavior>
+                            <a>Job Openings</a>
+                          </Link>
+                        </li>
+                        <li>
+                          <Link
+                            href="/footerpages/privacypolicy"
+                            legacyBehavior
+                          >
+                            <a>Privacy Policy</a>
+                          </Link>
+                        </li>
+                        <li>
+                          <Link
+                            href="/footerpages/school-tie-up"
+                            legacyBehavior
+                          >
+                            <a>School Tie-up</a>
+                          </Link>
+                        </li>
+                      </ul>
+                    </div>
+                  </div>
+                </div>
+                <div className="col-xl-2 col-lg-6 col-md-6 col-sm-6 col-6">
+                  <div className="single-widget">
+                    <div className="footer-title">
+                      <h3>Centres</h3>
+                    </div>
+                    <div className="footer-link">
+                      <ul>
+                        <li>
+                          <Link href="/about/saltlake">Saltlake</Link>
+                        </li>
+                        <li>
+                          <Link href="/about/phoolbagan" legacyBehavior>
+                            <a>Phoolbagan</a>
+                          </Link>
+                        </li>
+                        <li>
+                          <Link href="/about/howrah" legacyBehavior>
+                            <a>Howrah</a>
+                          </Link>
+                        </li>
+                        <li>
+                          <Link href="/about/andul" legacyBehavior>
+                            <a>Andul</a>
+                          </Link>
+                        </li>
+                        <li>
+                          <Link href="/about/Serampore" legacyBehavior>
+                            <a>Serampore</a>
+                          </Link>
+                        </li>
+                        <li>
+                          <Link href="/about/chandannagar" legacyBehavior>
+                            <a>Chandannagar</a>
+                          </Link>
+                        </li>
+                        <li>
+                          <Link href="/about/mogra" legacyBehavior>
+                            <a>Mogra</a>
+                          </Link>
+                        </li>
+                        <li>
+                          <Link href="/about/Rampurhat" legacyBehavior>
+                            <a>Rampurhat</a>
+                          </Link>
+                        </li>
+                        <li>
+                          <Link href="/about/viproad" legacyBehavior>
+                            <a>VIP Road</a>
+                          </Link>
+                        </li>
+                        <li>
+                          <Link href="/about/dunlop" legacyBehavior>
+                            <a>Dunlop</a>
+                          </Link>
+                        </li>
+                        <li>
+                          <Link href="/about/Raiganj" legacyBehavior>
+                            <a>Raiganj</a>
+                          </Link>
+                        </li>
+                        <li>
+                          <Link href="/about/Balurghat" legacyBehavior>
+                            <a>Balurghat</a>
+                          </Link>
+                        </li>
+                        <li>
+                          <Link href="/about/Berhampore" legacyBehavior>
+                            <a>Berhampore</a>
+                          </Link>
+                        </li>
 
-                            <li><Link href="/about/agartala"legacyBehavior><a>Agartala</a></Link></li>
-                            <li><Link href="/about/gariahat"legacyBehavior><a>Gariahat</a></Link></li>
-
-                        </ul>
+                        <li>
+                          <Link href="/about/agartala" legacyBehavior>
+                            <a>Agartala</a>
+                          </Link>
+                        </li>
+                        <li>
+                          <Link href="/about/gariahat" legacyBehavior>
+                            <a>Gariahat</a>
+                          </Link>
+                        </li>
+                      </ul>
                     </div>
+                  </div>
                 </div>
-            </div>
-            <div className="col-xl-2 col-lg-6 col-md-6 col-sm-6 col-6"> 
-                <div className="single-widget">
+                <div className="col-xl-2 col-lg-6 col-md-6 col-sm-6 col-6">
+                  <div className="single-widget">
                     <div className="footer-title">
-                        <h3>Contact</h3>
+                      <h3>Contact</h3>
                     </div>
                     <div className="contact-info">
-                        <ul>
-                            <li><a href="tel:+919051509444">Call : <span>9051509444</span></a></li>
-                            <li><a href="mailto:zenithinsti@gmail.com">Email : <span>zenithinsti@gmail.com</span></a></li>
-                            <li>Address : <span>India</span></li>
-                        </ul>
+                      <ul>
+                        <li>
+                          <a href="tel:+919051509444">
+                            Call : <span>9051509444</span>
+                          </a>
+                        </li>
+                        <li>
+                          <a href="mailto:zenithinsti@gmail.com">
+                            Email : <span>zenithinsti@gmail.com</span>
+                          </a>
+                        </li>
+                        <li>
+                          Address : <span>India</span>
+                        </li>
+                      </ul>
                     </div>
+                  </div>
                 </div>
+              </div>
             </div>
-        </div>
-        </div>
-        </div>
+          </div>
         </div>
         <div className="row">
-        <div className="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
-        <div className="copy-right-area">
-        <p className="copy-text">{FooterConstant.copyrightText}</p>
+          <div className="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
+            <div className="copy-right-area">
+              <p className="copy-text">{FooterConstant.copyrightText}</p>
+            </div>
+          </div>
         </div>
-        </div>
-        </div>
-        </div>
-        <img className="shape hrrd-footer-shape" src="/assets/images/shape/hrrd-footer.png" alt=""/>
+      </div>
+      <img
+        className="shape hrrd-footer-shape"
+        src="/assets/images/shape/hrrd-footer.png"
+        alt=""
+      />
     </div>
-);
-}
-export default Footer
+  );
+};
+export default Footer;
